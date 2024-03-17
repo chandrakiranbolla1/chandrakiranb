bdbca67ef1074a36e560df2af87e764a6c80b3f0
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-03-16 19:40:10 -0500

    initial-messgae

---
+++ src/app2.java
@@ -0,0 +1,11 @@
+public class app2 
+{
+
+    public static void main(String args[])
+    {
+        int a = 20;
+        int b = 30;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
