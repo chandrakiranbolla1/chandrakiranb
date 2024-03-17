2cf1205e3e8aefe72557ea91dbd280e674dd1c91
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-03-16 19:48:37 -0500

    test2

---
+++ src/app3.java
@@ -0,0 +1,12 @@
+public class app3 
+{
+
+    public static void main(String a[])
+    {
+        int c = 30;
+        int d = 30;
+        int result = c + d;
+        System.out.println(result);
+    }
+    
+}
