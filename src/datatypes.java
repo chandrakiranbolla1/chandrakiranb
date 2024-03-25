6cca38795ea96196a2c9d4a1d826744d9cfc4521
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-03-25 04:02:57 -0500

    data types

---
+++ src/datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes 
+{
+    public static void main(String a[])
+    {
+        int num  = 1;
+        byte by  = 127;
+        short sh = 558;
+        long l   = 1234l;
+        float f  = 5.6f;
+        double d = 5.8;
+
+        char c = 'B';
+
+        boolean b = true;
+    }
+    
+}
