10302ad1f72d56508a23605a849f28d4d51cea13
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-04-22 11:28:43 -0500

    typecasting-demo

---
+++ src/typecasting.java
@@ -0,0 +1,10 @@
+public class typecasting {
+    public static void main(String a[])
+    {
+        int alpha = 51;
+        int beta = 25;
+        alpha = beta;
+        beta++;
+        System.out.println(beta);
+    }    
+}
