3694a0afaa0bbef6ea9cdb86d0be8e85eaa7d94a
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-04-21 21:50:42 -0500

    literals

---
+++ src/literals01.java
@@ -0,0 +1,31 @@
+public class literals01 {
+    public static void main(String[] args) 
+    {
+        //literals
+        
+        //Hexa-decimal-number
+        int num1 = 0x7E;
+        System.out.println(num1);
+        
+        //to differentaite numbers
+        int num2 = 10_000_000;
+        System.out.println(num2);
+
+        //epsolon
+
+        double num3 = 12e10;
+        System.out.println(num3);
+
+        //char
+
+        char c = 'a';
+        System.out.println(c);
+
+        //char increment
+        char d ='a';
+        d++;
+        System.out.println(d);
+    }
+    
+    
+}
