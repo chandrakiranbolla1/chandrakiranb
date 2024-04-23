233ca17aa801ba03766522f85b2c14fc5f6cb3c3
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-04-23 17:22:30 -0500

    type-conversion

--- src/typecasting.java
+++ src/typecasting.java
@@ -4,7 +4,19 @@ public class typecasting {
         int alpha = 51;
         int beta = 25;
         alpha = beta;
+        int gama = ' ';
         beta++;
-        System.out.println(beta);
+        System.out.println(alpha);
+        System.out.println(gama);
+
+        int c = 257;
+        float d =  5.64f;
+        int t = (int) d;
+        System.out.println(t);
+
+        byte x = 10;
+        byte y = 20;
+        int result = x * y;
+        System.out.println(result);
     }    
 }
