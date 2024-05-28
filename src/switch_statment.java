4c5038cbdcf3035944b0e14bdc5f6f45f970db98
Chandra Kiran Bolla <pstuff979@gmail.com>
2024-05-28 12:34:27 -0400

    switch-statment

---
+++ src/switch_statment.java
@@ -0,0 +1,35 @@
+public class switch_statment {
+    public static void main(String args[]){
+        
+        int n = 9;
+
+        switch(n){
+            case 1:
+                System.out.println("Violet");
+                break;
+            case 2:
+                System.out.println("Indigo");
+                break;
+            case 3:
+                System.out.println("Blue");
+                break;
+            case 4:
+                System.out.println("Green");
+                break;
+            case 5:
+                System.out.println("Yellow");
+                break;
+            case 6:
+                System.out.println("Orange");
+                break;
+            case 7:
+                System.out.println("Red");
+                break;
+            default:
+                System.out.println("\n'This Particular color is out of the 7 colors'");
+
+        }
+            
+    }
+    
+}
