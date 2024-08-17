38e02d4b78c88ec657fe046ac6825307aad75e37
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-08-17 19:43:46 -0400

    static keyword added

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@ class Mobile
 {
     String brand;
     int price;
-    String name;
+    static String name;
 
     public void show()
     {
@@ -17,12 +17,12 @@ public class Demo
         Mobile obj1 = new Mobile();
         obj1.brand = "Apple";
         obj1.price = 1500;
-        obj1.name = "smart phone";
+        Mobile.name = "smart phone";
 
         Mobile obj2 = new Mobile();
         obj2.brand = "Samsung";
         obj2.price = 1200;
-        obj2.name = "smart phone";
+        Mobile.name = "smart phone";
 
         obj1.show();
         obj2.show();
