646efdf7bca246cf368df68073cd13d653083212
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-08-18 16:59:47 -0400

    static and Reference added.

--- src/Demo.java
+++ src/Demo.java
@@ -1,31 +1,56 @@
+// we cannot make a non-static method with the help of a class name
+// Static method can be called directly with the help of a class name
+// Static variable can be used inside a static method, we cannot use a non-static variable inside the static method
+
+
 class Mobile
 {
     String brand;
     int price;
     static String name;
 
-    public void show()
+    static {
+        name = "phone";
+        System.out.println("in Static Block");
+    }
+
+    Mobile()
     {
-        System.out.println(brand + " : " + price + " : " + name);
+        brand = "";
+        price = 1500;
+        System.out.println("in constructor");
+    }
+
+    void show(){
+        System.out.println( name + " " + brand + " " + price);
+    }
+
+    public static void show1(Mobile obj1){
+        System.out.println(name + " " + obj1.brand + " " + obj1.price);
     }
 }
 
-public class Demo
-{
-    public static void main(String[] args)
-    {
+
+class Demo{
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class.forName("Mobile");
         Mobile obj1 = new Mobile();
-        obj1.brand = "Apple";
         obj1.price = 1500;
-        Mobile.name = "smart phone";
+        obj1.brand = "Apple";
+        Mobile.name = "Phone";
 
         Mobile obj2 = new Mobile();
-        obj2.brand = "Samsung";
         obj2.price = 1200;
-        Mobile.name = "smart phone";
+        Mobile.name = "SmartPhone";
+        obj2.brand = "Samsung";
+
+        obj1.name = "Phone";
 
         obj1.show();
         obj2.show();
+
+        Mobile.show1(obj1);
 
     }
 }
