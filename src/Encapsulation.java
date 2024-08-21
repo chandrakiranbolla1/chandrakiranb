d96d3decbe84b48b95ca953c49bec5aa72bd0daf
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-08-21 14:50:35 -0400

    Encapsulation with the context and Getters and setters

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -1,12 +1,24 @@
+// Method name should define what is your intention,Most of the time it is not about writing code it is also
+// about reading code.  getName, setName are considered standards
+// variable getName is getter
+// Variable setName is Setter
+
 class Human{
-    private String name = "ChandraKiran_Bolla";
-    private int age = 25;
+    private String name;
+    private int age;
 
     public String getName()
     {
         return name;
     }
 
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
     public int getAge()
     {
         return age;
@@ -18,8 +30,10 @@ public class Encapsulation {
 
     public static void main(String[] args) {
         Human obj = new Human();
+        obj.setAge(25);
+        obj.setName("ChandraKiran_Bolla");
 
-        System.out.println(obj.getName() +" " +obj.getAge() );
+        System.out.println(obj.getName() + " : " + obj.getAge());
 
     }
 }
