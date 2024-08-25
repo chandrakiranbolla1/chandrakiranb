5aef8b9ec2e4ef3aa48aa3c96f3f3e09e1d8ed6b
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-08-24 20:39:46 -0400

    Anonymous object and their calling.

---
+++ src/Anonymous.java
@@ -0,0 +1,23 @@
+class C
+{
+    public C()
+    {
+        System.out.println("object created");
+    }
+    public void show()
+    {
+        System.out.println("in show");
+    }
+}
+
+public class Anonymous {
+    public static void main(String[] args) {
+//        This is the tradational method for object creation where obj is a refrence varibale in stack memory
+//        C obj = new C();
+//        obj.show();
+        //Anonymous object can be called only once
+        new C();  // This is to create an Anonymous object
+        new C().show();  // and this is to call both objects and its methods
+
+    }
+}
