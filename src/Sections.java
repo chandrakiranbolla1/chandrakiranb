76843b891084aecf2ceb4003cdada12affb91ba5
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-09-08 19:52:38 -0400

    commit

---
+++ src/Sections.java
@@ -0,0 +1,12 @@
+import Gat.*;
+
+
+public class Sections {
+    public String SectionsString(String Sections){
+        return "No.Of Sections are: ";
+    }
+
+    public int Sectionsint(int N1){
+        return N1;
+    }
+}
