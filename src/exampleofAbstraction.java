5f1db7312adfdd1892181ded8b776a871b5f4156
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-09-21 22:20:22 -0500

    Abstraction another example added.

--- src/exampleofAbstraction.java
+++ src/exampleofAbstraction.java
@@ -37,3 +37,32 @@ public class exampleofAbstraction
     }
 
 }
+
+// Abstract class
+abstract class Animal
+{
+    public abstract void animalSound();
+
+    public void sleep()
+    {
+        System.out.println("zzz");
+    }
+}
+
+// Subclass (inherit from Animal)
+
+ class Pig extends Animal
+ {
+     public void animalSound()
+     {
+         System.out.println("The pig says: wee wee");
+     }
+ }
+
+ class Main{
+     public static void main(String[] args) {
+         Pig myPig = new Pig();
+         myPig.animalSound();
+         myPig.sleep();
+     }
+ }
