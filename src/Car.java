7f714fe79106e6394aef48360ee8642a1c85da7b
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-10-10 20:28:07 -0500

    example class for "this" keyword

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    private String make;
+    private String model;
+    private int year;
+
+    Car(String make,String model,int year){
+       this.make = make;
+       this.model = model;
+       this.year = year;
+    }
+
+}
+
