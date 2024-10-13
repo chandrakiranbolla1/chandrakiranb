8542b87a44b166c360abaa41d29e67f8c6534095
chandrakiranbolla1 <bollachandrakiran3@gmail.com>
2024-10-13 16:15:11 -0500

    using lambda expression

--- src/ThreadsExample.java
+++ src/ThreadsExample.java
@@ -1,51 +1,19 @@
-class Greeting{
-    public void run(){
-        System.out.println("Greeting everyone");
-    }
-}
-
-class Hi extends Greeting implements Runnable{
-    public void run(){
-        for (int i = 0;i<10;i++)
-        {
-            System.out.println("Hi");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-
-class Hello extends Greeting implements Runnable{
-    public void run(){
-        for (int i=0;i<10;i++)
-        {
-            System.out.println("Hello");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-    }
-}
-
-
 public class ThreadsExample {
     public static void main(String[] args) {
-        Runnable obj =  new Hi();
-        Hello obj1 = new Hello();
+        Runnable obj = () -> {
+                for (int i=0;i<5;i++)
+                    System.out.println("Hi");
+                try {Thread.sleep(10);} catch (InterruptedException e) {throw new RuntimeException(e);}
+            };
+        Runnable obj1 = () ->{
+            for (int i =0;i<5;i++)
+                System.out.println("Hello");
+            try {Thread.sleep(10);}catch (InterruptedException e) {throw new RuntimeException(e);}
+        };
 
         Thread t1 = new Thread(obj);
         Thread t2 = new Thread(obj1);
-        //using priority we are giving the scheduler the suggestion.
-        t1.setPriority(Thread.MAX_PRIORITY-1);
-        t2.setPriority(4);
-        //range of prioritry is 1-10 1 is least and 10 is the highest
-        System.out.println(t2.getPriority());
+
         t1.start();
         t2.start();
     }
